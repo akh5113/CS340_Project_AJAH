6ba2a009472084395b4f9f9f1af6a7d55ba8cece
Anne Harris <20761577+akh5113@users.noreply.github.com>
2018-10-05 15:59:07 -0400

    Updated tables, added many-to-many
    I downloaded the file, updated the tables and the foreign keys, add the table for the many-to-many relationship. Decided to upload it as a new file

---
+++ Project_Step2_AJAH.sql
@@ -0,0 +1,100 @@
+/*
+CS340 Project Group 15
+Anne Harris
+Aaron Johnson
+Project Step 2
+*/
+
+--
+-- Table structure for table `olympic_games`
+--
+
+DROP TABLE IF EXISTS `olympic_games`;
+CREATE TABLE `olympic_games`(
+`id` int(11) NOT NULL AUTO_INCREMENT,
+`games_year` int(4) NOT NULL,
+`season` tinyint(1) NOT NULL,
+`country` varchar(100) NOT NULL,
+`city` varchar(100)
+PRIMARY KEY(`id`) 
+)ENGINE=InnoDB;
+
+--
+-- Data for table `olympic_games`
+--
+
+LOCK TABLES `olympic_games` WRITE;
+INSERT INTO `olympic_games` VALUES (1, 2018, 0, 'South Korea', 'PyeongChang'),
+								   (2, 2016, 1, 'Brazil', 'Reo de Janeiro'),
+								   (3, 2014, 0, 'Russia', 'Sochi'),
+								   (4, 2012, 1, 'United Kingdom', 'London');
+UNLOCK TABLES;
+
+--
+-- Table structure for table `events`
+--
+
+DROP TABLE IF EXISTS `events`;
+CREATE TABLE `events`(
+`id` int(11) NOT NULL AUTO_INCREMENT,
+`name` varchar(100) NOT NULL,
+`goldWinner` int(11),
+`goldTime` TIME,
+`gender` tinyint(1) NOT NULL,
+`olympicID` int(11) NOT NULL,
+PRIMARY KEY(`id`)
+FOREIGN KEY (`olympicID`) REFERENCES `olympic_games` (`id`),
+FOREIGN KEY (`goldWinner`) REFERENCES `athletes` (`id`)
+)ENGINE=InnoDB;
+
+--
+-- Data for table `events`
+--
+
+LOCK TABLES `events`;
+INSERT INTO `events` VALUES (1, '100 Meter Sprint', 1, '00:00:20', 2),
+
+-- 
+-- Table structure for table `athletes`
+--
+
+DROP TABLE IF EXISTS `athletes`;
+CREATE TABLE `athletes`(
+`id` int(11) NOT NULL AUTO_INCREMENT,
+`firstName` varchar(50) NOT NULL,
+`lastName` varchar(50) NOT NULL,
+`teamID` int(11) NOT NULL,
+`gender` tinyint(1) NOT NULL,
+PRIMARY KEY(`id`),
+FOREIGN KEY(`teamID`) REFERENCES `teams`(`id`)
+)ENGINE=InnoDB;
+
+--
+-- Table structre for table `teams`
+--
+
+DROP TABLE IF EXISTS `teams`;
+CREATE TABLE `teams`(
+`id` int(11) NOT NULL AUTO_INCREMENT,
+`name` varchar(100) NOT NULL,
+`numAthletes` int(11) DEFAULT '1',
+`goldMedals` int(11) DEFAULT '0',
+`olympicID` int(11) NOT NULL,
+PRIMARY KEY(`id`),
+FOREIGN KEY(`olympicID`) REFERENCES `olympic_games`(`id`)
+)ENGINE=InnoDB;
+
+--
+-- Table structure for many-to-many relationship
+-- Utalizes the id from the althletes table and the id from the events table
+-- 
+
+DROP TABLE IF EXISTS `has`;
+CREATE TABLE `has`(
+`athleteID` int(11) NOT NULL,
+`eventID` int(11) NOT NULL,
+PRIMARY KEY(`athleteID`, `eventID`),
+FOREIGN KEY(`athleteID`) REFERENCES `athletes`(`id`),
+FOREIGN KEY(`eventID`) REFERENCES `events`(`id`)
+)ENGINE=InnoDB;
+
