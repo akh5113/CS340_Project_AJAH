b8b60441ef0844c938341ff0e2854b82e7f11c72
Aaron Johnson <johnsaar@flip2.engr.oregonstate.edu>
2018-10-09 09:44:41 -0700

    Fixed syntax so code will import, need to double check key constraints

--- Project_Step2_AJAH.sql
+++ Project_Step2_AJAH.sql
@@ -8,14 +8,20 @@
 -- Table structure for table `olympic_games`
 --
 
+DROP TABLE IF EXISTS `has`;
+DROP TABLE IF EXISTS `events`;
+DROP TABLE IF EXISTS `athletes`;
+DROP TABLE IF EXISTS `teams`;
 DROP TABLE IF EXISTS `olympic_games`;
+
 CREATE TABLE `olympic_games`(
 `id` int(11) NOT NULL AUTO_INCREMENT,
 `games_year` int(4) NOT NULL,
 `season` tinyint(1) NOT NULL,
 `country` varchar(100) NOT NULL,
 `city` varchar(100),
-PRIMARY KEY(`id`) 
+UNIQUE KEY `games_year` (`games_year`),
+PRIMARY KEY(`id`)
 )ENGINE=InnoDB;
 
 --
@@ -30,39 +36,36 @@ INSERT INTO `olympic_games` VALUES (1, 2018, 0, 'South Korea', 'PyeongChang'),
 UNLOCK TABLES;
 
 --
--- Table structure for table `events`
+-- Table structure for table `teams`
 --
 
-DROP TABLE IF EXISTS `events`;
-CREATE TABLE `events`(
+CREATE TABLE `teams`(
 `id` int(11) NOT NULL AUTO_INCREMENT,
 `name` varchar(100) NOT NULL,
-`goldWinner` int(11),
-`goldTime` TIME,
-`gender` tinyint(1) NOT NULL,
+`numAthletes` int(11) DEFAULT '1',
+`goldMedals` int(11) DEFAULT '0',
 `olympicID` int(11) NOT NULL,
 PRIMARY KEY(`id`),
-FOREIGN KEY (`olympicID`) REFERENCES `olympic_games` (`id`),
-FOREIGN KEY (`goldWinner`) REFERENCES `athletes` (`id`)
+KEY `olympicID`(`olympicID`),
+	CONSTRAINT `teams_ibfk_1` FOREIGN KEY (`olympicID`) 
+		REFERENCES `olympic_games`(`id`) ON DELETE CASCADE
 )ENGINE=InnoDB;
 
 --
--- Data for table `events`
+-- Data for table `teams`
 --
 
-LOCK TABLES `events`;
-INSERT INTO `events` VALUES (1, 'Summer Event 0', 4, '00:00:20', 2),
-							(2, 'Winter Event 1', 6, '00:20:01', 1),
-							(3, 'Summer Event 2', 5, '00:00:12', 2),
-							(4, 'Winter Event 2', 3, '01:15:01', 3),
-							(5, 'Summer Event 0', 1, '00:00:19', 4);
+LOCK TABLES `teams` WRITE;
+INSERT INTO `teams` VALUES (1, 'USA', 2, 1, 4),
+						   (2, 'France', 1, 1, 3),
+						   (3, 'Brazil', 2, 2, 2),
+						   (4, 'USA', 2, 1, 1);
 UNLOCK TABLES;
- 
+
 -- 
 -- Table structure for table `athletes`
 --
 
-DROP TABLE IF EXISTS `athletes`;
 CREATE TABLE `athletes`(
 `id` int(11) NOT NULL AUTO_INCREMENT,
 `firstName` varchar(50) NOT NULL,
@@ -70,14 +73,16 @@ CREATE TABLE `athletes`(
 `teamID` int(11) NOT NULL,
 `gender` tinyint(1) NOT NULL,
 PRIMARY KEY(`id`),
-FOREIGN KEY(`teamID`) REFERENCES `teams`(`id`)
+KEY `teamID` (`teamID`),
+	CONSTRAINT `athletes_ibfk_1` FOREIGN KEY (`teamID`)
+		REFERENCES `teams`(`id`) ON DELETE CASCADE
 )ENGINE=InnoDB;
 
 --
 -- Data for table `athletes`
 --
 
-LOCK TABLES `athletes`;
+LOCK TABLES `athletes` WRITE;
 INSERT INTO `athletes` VALUES (1, 'Athlete1', 'Summer4', 1, 1),
 							  (2, 'Athlete2', 'Summer4', 1, 0),
 							  (3, 'Athlete1', 'Winter3', 2, 1),
@@ -88,50 +93,58 @@ INSERT INTO `athletes` VALUES (1, 'Athlete1', 'Summer4', 1, 1),
 UNLOCK TABLES;
 
 --
--- Table structure for table `teams`
+-- Table structure for table `events`
 --
 
-DROP TABLE IF EXISTS `teams`;
-CREATE TABLE `teams`(
+CREATE TABLE `events`(
 `id` int(11) NOT NULL AUTO_INCREMENT,
 `name` varchar(100) NOT NULL,
-`numAthletes` int(11) DEFAULT '1',
-`goldMedals` int(11) DEFAULT '0',
+`goldWinner` int(11) NOT NULL,
+`goldTime` TIME,
+`gender` tinyint(1) NOT NULL,
 `olympicID` int(11) NOT NULL,
 PRIMARY KEY(`id`),
-FOREIGN KEY(`olympicID`) REFERENCES `olympic_games`(`id`)
+KEY `olympicID` (`olympicID`),
+KEY `goldWinner` (`goldWinner`),
+	CONSTRAINT `events_ibfk_1` FOREIGN KEY (`olympicID`)
+		REFERENCES `olympic_games` (`id`) ON DELETE CASCADE,
+	CONSTRAINT `events_ibfk_2` FOREIGN KEY (`goldWinner`)
+		REFERENCES `athletes` (`id`) ON DELETE CASCADE
 )ENGINE=InnoDB;
 
 --
--- Data for table `teams`
+-- Data for table `events`
 --
 
-LOCK TABLES `teams`;
-INSERT INTO `teams` VALUES (1, 'USA', 2, 1, 4),
-						   (2, 'France', 1, 1, 3),
-						   (3, 'Brazil', 2, 2, 2),
-						   (4, 'USA', 2, 1, 1);
+LOCK TABLES `events` WRITE;
+INSERT INTO `events` VALUES (1, 'Summer Event 0', 4, '00:00:20', 0, 2),
+							(2, 'Winter Event 1', 6, '00:20:01', 1, 1),
+							(3, 'Summer Event 2', 5, '00:00:12', 0, 2),
+							(4, 'Winter Event 2', 3, '01:15:01', 1, 3),
+							(5, 'Summer Event 0', 1, '00:00:19', 1, 4);
 UNLOCK TABLES;
-
+ 
 --
 -- Table structure for many-to-many relationship
 -- Utilizes the id from the athletes table and the id from the events table
 -- 
 
-DROP TABLE IF EXISTS `has`;
 CREATE TABLE `has`(
 `athleteID` int(11) NOT NULL,
 `eventID` int(11) NOT NULL,
 PRIMARY KEY(`athleteID`, `eventID`),
-FOREIGN KEY(`athleteID`) REFERENCES `athletes`(`id`),
-FOREIGN KEY(`eventID`) REFERENCES `events`(`id`)
+KEY `eventID` (`eventID`),
+CONSTRAINT `has_ibfk_1` FOREIGN KEY (`athleteID`)
+	REFERENCES `athletes`(`id`) ON DELETE CASCADE,
+CONSTRAINT `has_ibfk_2` FOREIGN KEY (`eventID`)
+	REFERENCES `events`(`id`) ON DELETE CASCADE
 )ENGINE=InnoDB;
 
 --
 -- Data for table `has`;
 --
 
-LOCK TABLES `has`;
+LOCK TABLES `has` WRITE;
 INSERT INTO `has` VALUES (1, 5), (2, 5), (3, 4), (4, 1), (5, 3), (6, 2), (7, 2);
 UNLOCK TABLES;
 
