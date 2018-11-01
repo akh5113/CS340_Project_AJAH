1c0f07cdce80668d20a18d0a80801881b9c69e37
Anne Harris <20761577+akh5113@users.noreply.github.com>
2018-11-01 19:39:15 -0400

    added router info

--- view_games.js
+++ view_games.js
@@ -4,40 +4,45 @@ Anne Harris
 Aaron Johnson
 *********************/
 
+module.exports = function(){
+    var express = require('express');
+    var router = express.Router();
 
-
-/*get all alien games for dropdown */
-//NOTE: edited sql
-function getGamesDropdown(res, mysql, context, complete){
-	var sql = "SELECT games_year AS Year, country, city, IF(season = 1, 'Summer', 'Winter') AS Season FROM alien_games";
-	mysql.pool.query(sql, function(error, results, fields){
-		if(error){
-                res.write(JSON.stringify(error));
-                res.end();
-        }
-        context.alien_games = results;
-        complete();
-	});
-}
-
-/*get all 
-
-/*Display all Alien Games, display all Athletes, display all Events, display all Teams */
- 
-router.get('/', function(req, res){
-	var callbackCount = 0;
-	var context = {};
-	//context.jsscripts = 
-	var mysql = req.app.get('mysql');
-	getGamesDropdown(res, mysql, context, complete);
-	getGames(res, mysql, context, complete);
-	getAthletes(res, mysql, context, complete);
-	getEvents(res, mysql, context, complete);
-	getTeams(res, mysql, context, complete);
-	function complete(){
-		callbackCount++;
-		if(callbackCount >=2){
-			res.render('view_games', context)
+	/*get all alien games for dropdown */
+	//NOTE: edited sql
+	function getGamesDropdown(res, mysql, context, complete){
+		var sql = "SELECT games_year AS Year, country, city, IF(season = 1, 'Summer', 'Winter') AS Season FROM alien_games";
+		mysql.pool.query(sql, function(error, results, fields){
+			if(error){
+			res.write(JSON.stringify(error));
+			res.end();
 		}
+		context.alien_games = results;
+		complete();
+		});
 	}
-});
+
+	/*get all 
+
+	/*Display all Alien Games, display all Athletes, display all Events, display all Teams */
+
+	router.get('/', function(req, res){
+		var callbackCount = 0;
+		var context = {};
+		//context.jsscripts = 
+		var mysql = req.app.get('mysql');
+		getGamesDropdown(res, mysql, context, complete);
+		getGames(res, mysql, context, complete);
+		getAthletes(res, mysql, context, complete);
+		getEvents(res, mysql, context, complete);
+		getTeams(res, mysql, context, complete);
+		function complete(){
+			callbackCount++;
+			if(callbackCount >=2){
+				res.render('view_games', context)
+			}
+		}
+	});
+
+	return router;
+}();
