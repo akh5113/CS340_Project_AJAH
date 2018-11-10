27ce1be6d4abe41f90a0465f55c4b48fd859f330
Annie <annekharris@gmail.com>
2018-11-09 20:21:01 -0500

    Update public/delete_athlete.js

--- public/delete_athlete.js
+++ public/delete_athlete.js
@@ -1,6 +1,6 @@
-function deleteAthlete(id){
+function deleteAthlete(ID){
 	$.ajax({
-		url: '/edit_games/' + id,
+		url: '/edit_games/' + ID,
 		type: 'DELETE',
 		success:function(result){
 			window.location.reload(true);
